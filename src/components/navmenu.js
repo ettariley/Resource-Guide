2c5d374bbee89b30f81155fb92127022a8e5ea5f
Etta Baker <etta.r.baker@gmail.com>
2022-09-10 17:31:56 -0400

    Fixed nav links

--- src/components/navmenu.js
+++ src/components/navmenu.js
@@ -2,18 +2,19 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from "react-router-dom";
 
 function NavMenu() {
   return (
-    <Navbar variant="dark" bg='dark' expand="lg">
+    <Navbar fixed='top' variant="dark" bg='secondary' expand="md">
       <Container>
         <Navbar.Brand href="/">Hamblen Resource Guide</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Resources</Nav.Link>
-            <Nav.Link href="/events">Events</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
+            <Link className="nav-link" to="/">Resources</Link>
+            <Link className="nav-link" to="/events">Events</Link>
+            <Link className="nav-link" to="/about">About</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
