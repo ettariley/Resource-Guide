4873ebecb2087cfb9339a3ae82c69f9ba681d706
Etta Baker <etta.r.baker@gmail.com>
2022-09-10 17:31:21 -0400

    Added page stylings

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Fade from 'react-bootstrap/Fade';
 import { mockAboutText, mockAboutPartners } from '../mock-data';
+import './about.css';
 
 function About() {
+  const [open, setOpen] = useState(false);
+
   let partners = mockAboutPartners;
   let aboutText = mockAboutText;
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setOpen(true);
+  });
+
   return (
-    <Container>
-      <h2>About</h2>
-      <Row>
-        {aboutText.map(text => (
-          <Col>
-            {text.text}
-          </Col>
-        ))}
-      </Row>
-      <Row>
-        <h3>Partners</h3>
-          {partners.map(p => (
-            <Col key={p.name}>
-              <Button variant='link' href={p.link} target='_blank'>{p.name}</Button>
+    <Fade in={open}>
+      <Container className='about'>
+        <h2>About</h2>
+        <Row>
+          {aboutText.map(text => (
+            <Col>
+              {text.text}
             </Col>
           ))}
-      </Row>
-    </Container>
+        </Row>
+        <Row>
+          <h3 className='pt-2'>Partners</h3>
+            {partners.map(p => (
+              <Col md='auto' key={p.name}>
+                <Button variant='secondary' size='lg' href={p.link} target='_blank'>{p.name}</Button>
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    </Fade>
   );
 }
 
--- src/components/events/events.js
+++ src/components/events/events.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Fade from 'react-bootstrap/Fade';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './events.css';
 
 function Events() {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setOpen(true);
+  });
+
   return (
-    <Container>
-      <h2>Events</h2>
-      <Row>
-        <Col>This is the events page.</Col>
-      </Row>
-    </Container>
+    <Fade in={open}>
+      <Container className='events'>
+        <h2>Events</h2>
+        <Row>
+          <Col>This is the events page.</Col>
+        </Row>
+      </Container>
+    </Fade>
   );
 }
 
