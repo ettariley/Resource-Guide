a5a80dee6f42ff60f43a418d372191b050aa782f
Etta Baker <etta.r.baker@gmail.com>
2022-09-10 17:32:15 -0400

    Added features to resource page & cards

--- src/components/resource-card/resource-card.js
+++ src/components/resource-card/resource-card.js
@@ -1,37 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Modal from 'react-bootstrap/Modal';
+import './resource-card.css';
 
 function ResourceCard(props) {
   const { resource } = props;
+  const [show, setShow] = useState(false);
+
+  // Sort populations and services list for each resource (A-Z)
+  let resourcePop = resource.populationFilters.sort();
+  let resourceServ = resource.serviceFilters.sort();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <Card>
-      <Card.Body>
-        <Card.Title>{resource.provider}</Card.Title>
-        <Card.Text>{resource.description}</Card.Text>
-        <Card.Text>{resource.phone}</Card.Text>
-        <Card.Text>{resource.address}</Card.Text>
-        <h5>Services:</h5>
-        <ListGroup horizontal className='pb-3'>
-          {resource.serviceFilters.map(s => (
-            <ListGroup.Item variant='primary'>{s}</ListGroup.Item>
-          ))}
-        </ListGroup>
-        { (resource.populationFilters.length !== 0) ?
-          <>
-            <h5>Populations:</h5>
-            <ListGroup horizontal>
-              {resource.populationFilters.map(p => (
-                <ListGroup.Item variant='secondary'>{p}</ListGroup.Item>
-              ))}
+    <>
+      <Card>
+        <Card.Body className='text-bg-light'>
+          <Card.Title>{resource.provider}</Card.Title>
+          <Card.Subtitle className='mb-3'>{resource.address}</Card.Subtitle>
+          <Card.Text>
+            { (resource.website) ? 
+              <Card.Link href={resource.website} target="_blank">See Website</Card.Link>
+            : null }
+            <Card.Link href={`tel:`+resource.phone} target="_blank">Call {resource.phone}</Card.Link>
+          </Card.Text>
+          <Card.Text>{resource.description}</Card.Text>
+          <h5>Services:</h5>
+          {resourceServ.map(s => (
+            <ListGroup horizontal className='filter-list m-1'>
+              <ListGroup.Item variant='primary'>{s}</ListGroup.Item>
             </ListGroup>
-          </>
-          : null }
-        <Button variant="primary" href={resource.website} target="_blank" className='mt-3'>See Website</Button>
-      </Card.Body>
-    </Card>
+          ))}
+          { (resourcePop.length !== 0) ?
+            <>
+              <h5>Populations:</h5>
+                {resourcePop.map(p => (
+                  <ListGroup horizontal className='filter-list m-1'>
+                    <ListGroup.Item variant='secondary'>{p}</ListGroup.Item>
+                  </ListGroup>
+                ))}
+            </>
+            : null }
+          <Card.Text className='text-end'>
+            <Button variant='link' className='p-0' onClick={handleShow}><i class="bi bi-pencil-square"></i></Button>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      {/* Request an edit form modal */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title className='text-bg-light'>Request an Edit</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='text-bg-light'>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+    
   );
 }
 
--- src/components/resources/resources.js
+++ src/components/resources/resources.js
@@ -1,24 +1,74 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Fade from 'react-bootstrap/Fade';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Form from 'react-bootstrap/Form'
 import ResourceCard from '../resource-card/resource-card';
-import { mockResources } from '../mock-data';
+import { mockResources, mockServiceFilters, mockPopulationFilters } from '../mock-data';
+import './resources.css';
 
 function Resources() {
+  const [open, setOpen] = useState(false);
   let resources = mockResources;
+  let programFilters = mockServiceFilters.sort();
+  let populationFilters = mockPopulationFilters.sort();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    setOpen(true);
+  });
 
   return (
-    <Container className='pt-2'>
-      <h2>Resources</h2>
-      <Row>
-        {resources.map(r => (
+    <Fade in={open}>
+      <Container className='resources'>
+        <h2>Resources</h2>
+        <Row>
           <Col>
-            <ResourceCard resource={r} />
+            <h4>Featured Programs & Announcements</h4>
+            <p>Featured program and announcement information goes here.</p>
           </Col>
-        ))}
-      </Row>
-    </Container>
+        </Row>
+        <Row>
+          <h4>Search & Filter</h4>
+          {/* text search feature */}
+          <Col>
+            <Form.Control
+              type='text'
+              className='text-filter-form'
+              placeholder='Search Resources'
+            />
+          </Col>
+          {/* Program filter */}
+          <Col>
+            <DropdownButton variant='secondary' id='program-filter-dropdown' title='Programs'>
+              {programFilters.map(p => (
+                <Dropdown.Item href='#/action'>{p}</Dropdown.Item>
+              ))}
+            </DropdownButton>
+          </Col>
+          {/* Population filter */}
+          <Col>
+            <DropdownButton variant='secondary' id='population-filter-dropdown' title='Populations'>
+              {populationFilters.map(f => (
+                <Dropdown.Item href='#/action'>{f}</Dropdown.Item>
+              ))}
+            </DropdownButton>
+          </Col>
+        </Row>
+        {/* Resource List */}
+        <Row className='pt-2'>
+          <h4>Resource List</h4>
+          {resources.map(r => (
+            <Col>
+              <ResourceCard resource={r} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </Fade>
   );
 }
 
