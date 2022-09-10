6a54c0b85a2d55e7c350ea6fb4aebf0dc8bcde33
Etta Baker <etta.r.baker@gmail.com>
2022-09-10 17:30:40 -0400

    Added bootstrap icons & updated stylings

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from 'react';
 import {
-  BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
-import './App.css';
+import Container from 'react-bootstrap/Container';
 import NavMenu from './components/navmenu';
 import Footer from './components/footer/footer';
 import About from './components/about/about';
 import Events from './components/events/events';
 import Resources from './components/resources/resources';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter className="App">
-      <NavMenu />
-      <Routes>
-        <Route path="/" element={<Resources />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/events" element={<Events />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <Container fluid className="App d-flex flex-column justify-content-between">
+      <div>
+        <NavMenu />
+        <Routes>
+          <Route path="/" element={<Resources />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Events />} />
+        </Routes>
+      </div>
+      <Footer className='footer' />
+    </Container>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './custom.scss';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
