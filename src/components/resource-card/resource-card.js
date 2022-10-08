13f19a36fc874e87b531988fd08118f2cc4a6f73
Etta Baker <etta.r.baker@gmail.com>
2022-10-08 14:13:17 -0400

    Added tooltip for edit request button

--- src/components/resource-card/resource-card.js
+++ src/components/resource-card/resource-card.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Modal from 'react-bootstrap/Modal';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
 import './resource-card.css';
 
 function ResourceCard(props) {
@@ -19,43 +21,56 @@ function ResourceCard(props) {
   return (
     <>
       <Card>
-        <Card.Body className='text-bg-light'>
+        <Card.Body className="text-bg-light">
           <Card.Title>{resource.provider}</Card.Title>
-          <Card.Subtitle className='mb-3'>{resource.address}</Card.Subtitle>
+          <Card.Subtitle className="mb-3">{resource.address}</Card.Subtitle>
           <Card.Text>
-            { (resource.website) ? 
-              <Card.Link href={resource.website} target="_blank">See Website</Card.Link>
-            : null }
-            <Card.Link href={`tel:`+resource.phone} target="_blank">Call {resource.phone}</Card.Link>
+            {resource.website ? (
+              <Card.Link href={resource.website} target="_blank">
+                See Website
+              </Card.Link>
+            ) : null}
+            <Card.Link href={`tel:` + resource.phone} target="_blank">
+              Call {resource.phone}
+            </Card.Link>
           </Card.Text>
           <Card.Text>{resource.description}</Card.Text>
           <h5>Services:</h5>
-          {resourceServ.map(s => (
-            <ListGroup horizontal className='filter-list m-1'>
-              <ListGroup.Item variant='primary'>{s}</ListGroup.Item>
+          {resourceServ.map((s) => (
+            <ListGroup horizontal className="filter-list m-1">
+              <ListGroup.Item variant="primary">{s}</ListGroup.Item>
             </ListGroup>
           ))}
-          { (resourcePop.length !== 0) ?
+          {resourcePop.length !== 0 ? (
             <>
               <h5>Populations:</h5>
-                {resourcePop.map(p => (
-                  <ListGroup horizontal className='filter-list m-1'>
-                    <ListGroup.Item variant='secondary'>{p}</ListGroup.Item>
-                  </ListGroup>
-                ))}
+              {resourcePop.map((p) => (
+                <ListGroup horizontal className="filter-list m-1">
+                  <ListGroup.Item variant="secondary">{p}</ListGroup.Item>
+                </ListGroup>
+              ))}
             </>
-            : null }
-          <Card.Text className='text-end'>
-            <Button variant='link' className='p-0' onClick={handleShow}><i class="bi bi-pencil-square"></i></Button>
+          ) : null}
+          <Card.Text className="text-end">
+            <OverlayTrigger
+              placement="bottom"
+              overlay={<Tooltip>Request an edit</Tooltip>}
+            >
+              <Button variant="link" className="p-0" onClick={handleShow}>
+                <i class="bi bi-pencil-square"></i>
+              </Button>
+            </OverlayTrigger>
           </Card.Text>
         </Card.Body>
       </Card>
       {/* Request an edit form modal */}
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title className='text-bg-light'>Request an Edit</Modal.Title>
+          <Modal.Title className="text-bg-light">Request an Edit</Modal.Title>
         </Modal.Header>
-        <Modal.Body className='text-bg-light'>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body className="text-bg-light">
+          Woohoo, you're reading this text in a modal!
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
@@ -66,7 +81,6 @@ function ResourceCard(props) {
         </Modal.Footer>
       </Modal>
     </>
-    
   );
 }
 
