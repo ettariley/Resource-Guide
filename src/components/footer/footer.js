c174260f1f11fd2db3b82c27d4bd4040161e684a
Etta Baker <etta.r.baker@gmail.com>
2022-10-08 14:12:07 -0400

    Added logos to nav & footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,18 +2,15 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import { NavLink } from 'react-router-dom';
 import './footer.css';
 
 function Footer() {
   return (
-    <Container fluid className='text-bg-dark bg-secondary'>
-      <Row className='p-3 justify-content-around'>
-        <Col sm md='3' className='m-1 me-2'>
-          <h4>Hamblen Resource Guide</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </Col>
-        <Col sm md='3' className='m-1'>
+    <Container fluid className="text-bg-dark bg-secondary">
+      <Row className="p-3 justify-content-around">
+        <Col sm md="3" className="m-1 me-2">
           <h4>Contact Us</h4>
           <Row>
             <a
@@ -25,19 +22,52 @@ function Footer() {
             </a>
           </Row>
           <Row>
-            <a href="tel:423-438-7569" title="Call">
-              423-438-7569
+            <a href="tel:423-586-9431" title="Call">
+              (423) 586-9431
             </a>
           </Row>
           <Row>
-            <p>Address Line 1<br></br>Address Line 2</p>
+            <p>
+              2450 S. Cumberland St.<br></br>Morristown, TN 37813
+            </p>
           </Row>
         </Col>
-        <Col sm md='3' className='m-1 ms-2'>
+        <Col md="5" className="m-1">
+          <h4>Hamblen Resource Guide</h4>
+          <h6>A partnership between</h6>
+          <Row>
+            <Col sm="auto">
+              <a href="https://www.hcexcell.org/ready-by-6-1" target="_blank">
+                <Image
+                  fluid
+                  className="footer-logos"
+                  src={require('../../assets/Rb6logotest.png')}
+                />
+              </a>
+            </Col>
+            <Col sm="auto">
+              <a href="https://www.mhcentralservices.org/" target="_blank">
+                <Image
+                  fluid
+                  className="footer-logos"
+                  src={require('../../assets/MHCSlogowhite.png')}
+                />
+              </a>
+            </Col>
+          </Row>
+        </Col>
+
+        <Col sm md="3" className="m-1 ms-2">
           <h4>Site Menu</h4>
-          <Row><NavLink to='/'>Resources</NavLink></Row>
-          <Row><NavLink to='/events'>Events</NavLink></Row>
-          <Row><NavLink to='/about'>About</NavLink></Row>
+          <Row>
+            <NavLink to="/">Resources</NavLink>
+          </Row>
+          <Row>
+            <NavLink to="/events">Events</NavLink>
+          </Row>
+          <Row>
+            <NavLink to="/about">About</NavLink>
+          </Row>
         </Col>
       </Row>
     </Container>
--- src/components/navmenu.js
+++ src/components/navmenu.js
@@ -2,19 +2,27 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function NavMenu() {
   return (
-    <Navbar fixed='top' variant="dark" bg='secondary' expand="md">
+    <Navbar fixed="top" variant="dark" bg="secondary" expand="md">
       <Container>
-        <Navbar.Brand href="/">Hamblen Resource Guide</Navbar.Brand>
+        <Navbar.Brand href="/">
+          <img src={require('../assets/HRGlogo.png')} height="60" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Link className="nav-link" to="/">Resources</Link>
-            <Link className="nav-link" to="/events">Events</Link>
-            <Link className="nav-link" to="/about">About</Link>
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav>
+            <Link className="nav-link" to="/">
+              Resources
+            </Link>
+            <Link className="nav-link" to="/events">
+              Events
+            </Link>
+            <Link className="nav-link" to="/about">
+              About
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
