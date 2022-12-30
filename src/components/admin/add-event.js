93f8b37a23274cb77b7024811e6f818da3de1c09
Etta Baker <etta.r.baker@gmail.com>
2022-12-30 01:10:47 -0500

    added alerts with form information

--- src/components/admin/add-event.js
+++ src/components/admin/add-event.js
@@ -1,30 +1,55 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function AddEvent() {
-  const [showAlert, setShowAlert] = useState(true);
+  const [showAlert, setShowAlert] = useState(false);
+  const location = useLocation();
+  const selected = location.state?.selected;
+  const requestInfo = useRef(Object.keys(selected).length);
+
+  useEffect(() => {
+    if (requestInfo.current > 0) {
+      setShowAlert(true);
+    }
+    
+    return () => {
+      requestInfo.current = 0;
+    };
+  }, []);
+
+  const closeAlert = () => {
+    setShowAlert(false);
+    requestInfo.current = 0;
+  };
 
   return (
     <Container className='mt-5 pt-5'>
       <h2>Add Event</h2>
       {showAlert ? (
-        <Alert variant="primary" onClose={() => setShowAlert(false)} dismissible>
-          <Alert.Heading>Resource Request Information</Alert.Heading>
-          <p>
-            Here's the information that was provided in the information. We need to build this part out to actually include this information.
-          </p>
+        <Alert variant="primary" onClose={() => closeAlert()} dismissible>
+          <Alert.Heading>New Event Request Information</Alert.Heading>
+          Title: {selected.title}<br></br>
+          Event Start: {selected.start.toDateString()}<br></br>
+          Event End: {selected.end.toDateString()}<br></br>
+          Host: {selected.eventHost}<br></br>
+          Description: {selected.description}<br></br>
+          Location: {selected.location}<br></br>
+          Phone: {selected.hostPhone}<br></br>
+          Event Link: {selected.eventLink}<br></br>
+          Person Requesting: {selected.identifier}<br></br>
+          Date Submitted: {selected.dateSubmitted.toDateString()}
       </Alert>
       ) : null}
       {/* Return to admin dashboard button */}
       <Row className='mt-5'>
         <Col className='ps-0'>
           <Button variant="outline-light" size='sm' as={Link} to="/admin">
-            <i class="bi bi-arrow-left"></i> Back to Admin Dashboard
+            <i className="bi bi-arrow-left"></i> Back to Admin Dashboard
           </Button>
         </Col>
       </Row>
--- src/components/admin/edit-resource.js
+++ src/components/admin/edit-resource.js
@@ -1,31 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-function EditResource(props) {
-  const { selected } = props;
-  const [showAlert, setShowAlert] = useState(true);
+function EditResource() {
+  const [showAlert, setShowAlert] = useState(false);
+  const location = useLocation();
+  const selected = location.state?.selected;
+  const requestInfo = useRef(Object.keys(selected).length);
+
+  useEffect(() => {
+    if (requestInfo.current > 0) {
+      setShowAlert(true);
+    }
+    
+    return () => {
+      requestInfo.current = 0;
+    };
+  }, []);
+
+  const closeAlert = () => {
+    setShowAlert(false);
+    requestInfo.current = 0;
+  };
 
   return (
     <Container className='mt-5 pt-5'>
       <h2>Edit Resource</h2>
       {showAlert ? (
-        <Alert variant="primary" onClose={() => setShowAlert(false)} dismissible>
-          <Alert.Heading>Resource Request Information</Alert.Heading>
-          <p>
-            Here's the information that was provided in the information. We need to build this part out to actually include this information.
-          </p>
+        <Alert variant="primary" onClose={() => closeAlert()} dismissible>
+          <Alert.Heading>Edit Resource Request Information</Alert.Heading>
+          Provider: {selected.provider}<br></br>
+          Person Requesting: {selected.identifier}<br></br>
+          Submission Contact Phone: {selected.phone}<br></br>
+          Edit Requested: {selected.editRequest}<br></br>
+          Date Submitted: {selected.dateSubmitted.toDateString()}<br></br>
       </Alert>
       ) : null}
       {/* Return to admin dashboard button */}
       <Row className='mt-5'>
         <Col className='ps-0'>
           <Button variant="outline-light" size='sm' as={Link} to="/admin">
-            <i class="bi bi-arrow-left"></i> Back to Admin Dashboard
+            <i className="bi bi-arrow-left"></i> Back to Admin Dashboard
           </Button>
         </Col>
       </Row>
