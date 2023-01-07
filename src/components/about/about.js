e9afe60c2d500eae3989f0f31ea7027509d52c4b
Etta Baker <etta.r.baker@gmail.com>
2023-01-07 14:16:19 -0500

    implementing caching

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -23,41 +23,55 @@ function About() {
     setOpen(true);
   }, []);
 
+  // set featured text
   useEffect(() => {
-    const aboutTextQuery = query(doc(db, 'About-Page', 'AboutText'));
-    const aboutTextSnapshot = getDoc(aboutTextQuery).then(
-      (aboutTextSnapshot) => {
-        setAboutText(aboutTextSnapshot.data().text);
-        console.log(aboutText);
+    if (navigator.onLine) {
+      const aboutTextQuery = query(doc(db, 'About-Page', 'AboutText'));
+      const aboutTextSnapshot = getDoc(aboutTextQuery).then(
+        (aboutTextSnapshot) => {
+          setAboutText(aboutTextSnapshot.data().text);
+          localStorage.setItem('aboutFeaturedText', JSON.stringify(aboutTextSnapshot.data().text));
+          // console.log(aboutText);
+        }
+      );
+    } else {
+      if (localStorage.getItem('aboutFeaturedText') !== '') {
+        setAboutText(JSON.parse(localStorage.getItem('aboutFeaturedText')));
+      } else {
+        alert("No internet connection; can't load featured text.");
       }
-    );
+    } 
   }, []);
 
+  // set about partners
   useEffect(() => {
-    const partnersQuery = query(doc(db, 'About-Page', 'AboutPartners'));
-    const partnersQuerySnapshot = getDoc(partnersQuery).then(
-      (partnersQuerySnapshot) => {
-        setPartners(partnersQuerySnapshot.data().partners);
+    if (navigator.onLine) {
+      const partnersQuery = query(doc(db, 'About-Page', 'AboutPartners'));
+      const partnersQuerySnapshot = getDoc(partnersQuery).then(
+        (partnersQuerySnapshot) => {
+          setPartners(partnersQuerySnapshot.data().partners);
+          localStorage.setItem('aboutPartners', JSON.stringify(partners));
+        }
+      );
+    } else {
+      if (localStorage.getItem('aboutPartners')) {
+        setAboutText(JSON.parse(localStorage.getItem('aboutPartners')));
+      } else {
+        alert("No internet connection; can't load partners.");
       }
-    );
+    } 
+    
   }, []);
 
   return (
     <Fade in={open}>
       <Container className="about">
         <h2>About</h2>
-        {/* as rows */}
-        {/* {aboutText.map((text) => (
-          <Row>
-            <p>{text.text}</p>
-          </Row>
-        ))} */}
         {aboutText.map((text) => (
           <Row>
             <p>{text}</p>
           </Row>
         ))}
-        {/* <Row>{aboutText}</Row> */}
         <Row>
           <h3 className="pt-2">Partners</h3>
           {partners.map((p) => (
